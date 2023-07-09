5c443de089aaefee6e0f19f4f7885e26ad954ffb
mehedy3107 <mhador111@gmail.com>
2023-07-09 15:14:49 +0600

    Login & Sign Up Page Completed

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,69 +1,95 @@
 import React from "react";
-import { Text, View, TextInput, TouchableOpacity, Button } from "react-native";
+import {
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import Feather from "react-native-vector-icons/Feather";
-import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
 
 export default function Login({ navigation }) {
   return (
-    <View className="max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
-      <View className="mx-4 mt-5">
-        <Text className="text-3xl font-semibold text-cyan-500 uppercase mb-1">
-          Login
-        </Text>
-
-        <Text className="text-base font-normal text-black mb-8">
-          Already have an account? Login here.
-        </Text>
-
-        <View className="my-5">
-          <View className="flex flex-row p-3  bg-white border-cyan-500 border-2  mb-3 rounded-lg">
-            <Ionicons name="md-mail-open-outline" size={30} color="#06b6d4" />
-            <TextInput
-              type="text"
-              className="w-full  text-sm ml-3 shadow-sm placeholder-black "
-              placeholder="Your email"
-            />
-          </View>
-
-          <View className="flex flex-row p-3  bg-white border-cyan-500 border-2  mb-3 rounded-lg">
-            <Feather name="lock" size={30} color="#06b6d4" />
-            <TextInput
-              type="text"
-              className="w-full  text-sm ml-3 shadow-sm placeholder-black "
-              placeholder="Your password"
-            />
-          </View>
-        </View>
-
-        <TouchableOpacity>
-          <Text className="text-lg bg-cyan-500 text-center py-3 text-white rounded-lg font-semibold">
-            Sign Up
+    <ScrollView>
+      <View className="max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+        <View className="mx-4 mt-5">
+          <Text className="text-3xl font-semibold text-black uppercase mb-1 text-center">
+            Log In
           </Text>
-        </TouchableOpacity>
 
-        {/* <View className="flex flex-row justify-evenly my-10">
-          <View className="flex flex-row items-center space-x-2 border-2 border-blue-600 p-4 rounded-xl  bg-blue-600">
-            <FontAwesomeIcon name="facebook" size={30} color="#fff" />
-            <Text className="text-white text-lg">Facebook</Text>
+          <View className="flex flex-row justify-center space-x-3  my-3">
+            <View className="border-2 border-gray-300 p-3 rounded-full shadow-lg bg-white ">
+              <FontAwesomeIcon name="twitter" size={25} color="#000" />
+            </View>
+
+            <View className=" border-2 border-gray-300 p-3 rounded-full shadow-lg bg-white ">
+              <FontAwesomeIcon name="facebook" size={25} color="#000" />
+            </View>
+
+            <View className=" border-2 border-gray-300 p-3 rounded-full shadow-lg bg-white ">
+              <FontAwesomeIcon name="google" size={25} color="#000" />
+            </View>
           </View>
 
-          <View className="flex flex-row items-center space-x-2 border-2 border-red-500 p-4 rounded-xl  bg-red-500">
-            <FontAwesomeIcon name="google" size={30} color="#fff" />
-            <Text className="text-white text-lg">Google</Text>
+          <View>
+            <Text className="text-center font-medium ">or use your email</Text>
           </View>
-        </View> */}
 
-        <Text className="text-base font-normal text-black mb-8 my-10">
-          Don't have an account?{" "}
-          <Text
-            className="text-cyan-500 font-bold"
-            onPress={() => navigation.navigate("SignUp")}
-          >
-            Sign Up Here
-          </Text>{" "}
-        </Text>
+          <View className="my-5">
+            <View>
+              <Text className="mb-1 text-base font-bold">Email</Text>
+              <View className="flex flex-row p-3  bg-white border-cyan-500 border-2  mb-4 rounded-lg">
+                <Ionicons
+                  name="md-mail-open-outline"
+                  size={30}
+                  color="#06b6d4"
+                />
+                <TextInput
+                  type="text"
+                  className="w-full  text-sm ml-3 shadow-sm placeholder-black "
+                  placeholder="Your email"
+                />
+              </View>
+            </View>
+
+            <View>
+              <Text className="mb-1 text-base font-bold">Password</Text>
+              <View className="flex flex-row p-3  bg-white border-cyan-500 border-2  mb-1 rounded-lg">
+                <Feather name="lock" size={30} color="#06b6d4" />
+                <TextInput
+                  type="text"
+                  className="w-full  text-sm ml-3 shadow-sm placeholder-black "
+                  placeholder="Your password"
+                />
+              </View>
+            </View>
+
+            <View>
+              <Text className="mb-4 text-base font-bold text-right">
+                Forget password?
+              </Text>
+            </View>
+          </View>
+
+          <TouchableOpacity>
+            <Text className="text-lg bg-cyan-500 text-center py-3 text-white rounded-lg font-semibold">
+              Sign Up
+            </Text>
+          </TouchableOpacity>
+
+          <Text className="text-base font-normal text-black mb-8 my-10">
+            Don't have an account?{" "}
+            <Text
+              className="text-cyan-500 font-bold"
+              onPress={() => navigation.navigate("SignUp")}
+            >
+              Sign Up Here
+            </Text>{" "}
+          </Text>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
--- src/pages/signUp.js
+++ src/pages/signUp.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { Text, View, TextInput, TouchableOpacity, Button } from "react-native";
+import {
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import Feather from "react-native-vector-icons/Feather";
 import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
@@ -7,74 +13,106 @@ import FontAwesomeIcon from "react-native-vector-icons/FontAwesome";
 
 const SignUp = ({ navigation }) => {
   return (
-    <View className="max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
-      <View className="mx-4 mt-5">
-        <Text className="text-3xl font-semibold text-cyan-500 uppercase mb-1">
-          Sign UP
-        </Text>
-
-        <Text className="text-base font-normal text-black mb-8">
-          Are you new here? Create an account for you.
-        </Text>
-
-        <View className="my-5">
-          <View className="flex flex-row p-3  bg-white border-cyan-500 border-2  mb-3 rounded-lg">
-            <FontAwesomeIcon name="user-o" size={30} color="#06b6d4" />
-            <TextInput
-              type="text"
-              className="w-full  text-sm ml-3 shadow-sm placeholder-black "
-              placeholder="Your username"
-            />
-          </View>
-
-          <View className="flex flex-row p-3  bg-white border-cyan-500 border-2  mb-3 rounded-lg">
-            <Ionicons name="md-mail-open-outline" size={30} color="#06b6d4" />
-            <TextInput
-              type="text"
-              className="w-full  text-sm ml-3 shadow-sm placeholder-black "
-              placeholder="Your email"
-            />
-          </View>
-
-          <View className="flex flex-row p-3  bg-white border-cyan-500 border-2  mb-3 rounded-lg">
-            <Feather name="lock" size={30} color="#06b6d4" />
-            <TextInput
-              type="text"
-              className="w-full  text-sm ml-3 shadow-sm placeholder-black "
-              placeholder="Your password"
-            />
-          </View>
-        </View>
-
-        <TouchableOpacity>
-          <Text className="text-lg bg-cyan-500 text-center py-3 text-white rounded-lg font-semibold">
-            Sign Up
+    <ScrollView>
+      <View className="max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+        <View className="mx-4">
+          <Text className="text-3xl font-semibold text-cyan-500 uppercase mb-1">
+            Sign UP
           </Text>
-        </TouchableOpacity>
 
-        <View className="flex flex-row justify-evenly my-10">
-          <View className="flex flex-row items-center space-x-2 border-2 border-blue-600 p-4 rounded-xl  bg-blue-600">
-            <FontAwesomeIcon name="facebook" size={30} color="#fff" />
-            <Text className="text-white text-lg">Facebook</Text>
+          <Text className="text-base font-normal text-black ">
+            Are you new here? Create an account for you.
+          </Text>
+
+          <View className="my-5">
+            <View className="my-5">
+              <View>
+                <Text className="mb-1 text-base font-bold">Username</Text>
+                <View className="flex flex-row p-3  bg-white border-cyan-500 border-2  mb-4 rounded-lg">
+                  <Ionicons
+                    name="md-mail-open-outline"
+                    size={30}
+                    color="#06b6d4"
+                  />
+                  <TextInput
+                    type="text"
+                    className="w-full  text-sm ml-3 shadow-sm placeholder-black "
+                    placeholder="Your username"
+                  />
+                </View>
+              </View>
+
+              <View>
+                <Text className="mb-1 text-base font-bold">Email</Text>
+                <View className="flex flex-row p-3  bg-white border-cyan-500 border-2  mb-4 rounded-lg">
+                  <Ionicons
+                    name="md-mail-open-outline"
+                    size={30}
+                    color="#06b6d4"
+                  />
+                  <TextInput
+                    type="password"
+                    className="w-full  text-sm ml-3 shadow-sm placeholder-black "
+                    placeholder="Your email"
+                  />
+                </View>
+              </View>
+
+              <View>
+                <Text className="mb-1 text-base font-bold">Password</Text>
+                <View className="flex flex-row p-3  bg-white border-cyan-500 border-2  mb-1 rounded-lg">
+                  <Feather name="lock" size={30} color="#06b6d4" />
+                  <TextInput
+                    type="text"
+                    className="w-full  text-sm ml-3 shadow-sm placeholder-black "
+                    placeholder="Your password"
+                  />
+                </View>
+              </View>
+
+              <View>
+                <Text className="text-base font-bold text-right">
+                  Forget password?
+                </Text>
+              </View>
+            </View>
           </View>
 
-          <View className="flex flex-row items-center space-x-2 border-2 border-red-500 p-4 rounded-xl  bg-red-500">
-            <FontAwesomeIcon name="google" size={30} color="#fff" />
-            <Text className="text-white text-lg">Google</Text>
+          <TouchableOpacity>
+            <Text className="text-lg bg-cyan-500 text-center py-3 text-white rounded-lg font-semibold">
+              Sign Up
+            </Text>
+          </TouchableOpacity>
+
+          <View className="flex flex-row justify-evenly my-5">
+            <View className="flex flex-row items-center mr-2 space-x-2 border-2 border-blue-300 p-2 rounded-xl  bg-blue-300">
+              <FontAwesomeIcon name="twitter" size={30} color="#fff" />
+              <Text className="text-white text-sm font-medium">Twitter</Text>
+            </View>
+
+            <View className="flex flex-row items-center mr-2 space-x-2 border-2 border-red-500 p-2 rounded-xl  bg-red-500">
+              <FontAwesomeIcon name="google" size={30} color="#fff" />
+              <Text className="text-white text-sm font-medium">Google</Text>
+            </View>
+
+            <View className="flex flex-row items-center space-x-2 border-2 border-blue-400 p-2 rounded-xl  bg-blue-400">
+              <FontAwesomeIcon name="facebook" size={30} color="#fff" />
+              <Text className="text-white text-sm font-medium">Facebook</Text>
+            </View>
           </View>
+
+          <Text className="text-base font-normal text-black mb-8">
+            Already have an account?{" "}
+            <Text
+              className="text-cyan-500 font-bold"
+              onPress={() => navigation.navigate("Login")}
+            >
+              Login Here
+            </Text>{" "}
+          </Text>
         </View>
-
-        <Text className="text-base font-normal text-black mb-8">
-          Already have an account?{" "}
-          <Text
-            className="text-cyan-500 font-bold"
-            onPress={() => navigation.navigate("Login")}
-          >
-            Login Here
-          </Text>{" "}
-        </Text>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
